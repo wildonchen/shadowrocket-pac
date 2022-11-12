b363973f93ef634b33db178595f19e6de17bcab9
wildonchen <me@chenqiwei.net>
2022-11-12 17:52:58 +0800

    第一次同步

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# shadowrocket
-shadowrocket GFWlist 配置文件
+# GFWlist
+
+根据 SSR GFWlist 生成的 shadowrocket 配置文件，用于IOS端。这和默认的配置文件有什么区别？这就需要知道什么是 PAC 了。
+
+> PAC就是让代理软件通过PAC代理规则文件 进行上网流量分流，比如让国内IP、域名的流量直连，让国外的IP、域名通过SSR代理连接。
+
+GFWlist.conf 是为了只让被墙域名使用代理，尽可能的避免使用代理。减少代理流量。
+
+如果你不知道这几个文件是什么。那么你只需要知道 GFWlist.conf 怎么用。
+
+## 怎么使用
+
+你需要复制这个URL：https://raw.githubusercontent.com/wildonchen/shadowrocket-GFWlist/main/GFWlist.conf
+
+
+
---
+++ toList.py
@@ -0,0 +1,53 @@
+# 本脚本用于SSR GFWlist pac文件转IOS版shadowrocket配置文件
+import re
+
+try:
+    # 打开SSR pac文件，请确保有按照SSR软件，以及生成了GFWlist pac文件
+    file_object = open('D:\Portable Application\ShadowsocksR-win-4.9.2\pac.txt','r')
+except:
+    print("pac.txt 文件不存在或路径错误")
+    exit()
+all_the_text = file_object.read()
+# 格式化文件并将rules存为进数组
+all_the_text = all_the_text.replace("\t","").replace("\n","")
+text = re.findall('(?<=var rules = \[).*?(?=\];)', all_the_text)
+text = text[0].replace("\"","")
+urlList = text.split(",")
+# 对rules做处理
+domain = []
+for i in range(0, len(urlList)):
+    # 删除过滤
+    if '@' not in urlList[i] and '.' in urlList[i]:
+        # 去除|，去除协议头
+        domain1 = urlList[i].replace("|","").replace("https://","").replace("http://","")
+        # 去除第一个.
+        if len(domain1) and domain1[0] == '.':
+            domain1 = domain1.replace(".", "", 1)
+        # 获取主域名
+        domain1 = domain1.split('/')[0]
+        if domain1 not in domain:
+            domain.append(domain1)
+file_object.close()
+
+if len(domain) == 0:
+    print("未能正确识别文件")
+    exit()
+con = ''
+for i in range(0, len(domain)):
+    # 拼接成 DOMAIN-SUFFIX,t.me,PROXY 这样的格式
+    con += '\n' + 'DOMAIN-SUFFIX,' +domain[i] + ',PROXY'
+
+try:
+    # 拼接文件，最终写入 GFWlist.conf
+    ff = open('GFWlist.conf','w')
+    with open('template.conf','r') as f:
+        line = f.readlines()
+        for line_list in line:
+            line_new =line_list.replace('\n','')
+            if line_new == '[Rule]':
+                line_new = line_new + '\n' + '# GFWlist' +  con
+            line_new = line_new  +'\n'
+            ff.write(line_new)
+except:
+    print("写入文件错误")
+    exit()
