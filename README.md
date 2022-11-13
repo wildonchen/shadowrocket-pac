3de89d7b99699f1ea59e3beaec1209f12b25ed19
wildonchen <me@chenqiwei.net>
2022-11-13 15:45:46 +0800

    添加时间注释

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # GFWlist
 
-根据 [GFWlist](https://github.com/gfwlist/gfwlist) 生成的 shadowrocket 配置文件，用于IOS端。这和默认的配置文件有什么区别？这就需要知道什么是 PAC 了。
+根据 [GFWlist](https://github.com/gfwlist/gfwlist) 生成的 Shadowrocket 配置文件，用于IOS端。这和默认的配置文件有什么区别？这就需要知道什么是 PAC 了。
 
 > PAC就是让代理软件通过PAC代理规则文件 进行上网流量分流，比如让国内IP、域名的流量直连，让国外的IP、域名通过SSR代理连接。
 
@@ -12,7 +12,7 @@ GFWlist.conf 是为了只让被墙域名使用代理，尽可能的避免使用�
 
 你需要先复制这个URL：https://raw.githubusercontent.com/wildonchen/shadowrocket-GFWlist/main/GFWlist.conf
 
-再打开IOS 端的 shadowrocket 软件，然后按照下图步骤操作即可：
+再打开IOS 端的 Shadowrocket 软件，然后按照下图步骤操作即可：
 
 ![demo](demo.gif)
 
@@ -22,13 +22,22 @@ GFWlist.conf 是为了只让被墙域名使用代理，尽可能的避免使用�
 
 ## toList.py
 
-如果你有这方便的兴趣，我想你能看懂里面的注释，这是一个转换脚本。GFWlist 转  shadowrocket 配置文件。
+如果你有这方便的兴趣，我想你能看懂里面的注释，这是一个转换脚本。用于 GFWlist 转  Shadowrocket 配置文件。
 
-令行执行 python toList.py
+```shell
+# 如果没有 requests 库则执行安装命令
+pip install requests
+
+# 如果没有 pybase64 库则执行安装命令
+pip install pybase64
+
+# 执行转换脚本
+python toList.py
+```
 
 即可在同目录生成最新的 GFWlist.conf 文件。
 
-template.conf 文件是一个 shadowrocket 配置模板，用于合并生成新的 GFWlist.conf 文件。当然你也可以编辑它进行自定义，合并后依然会存在。
+template.conf 文件是一个 Shadowrocket 配置模板，用于合并生成新的 GFWlist.conf 文件。当然你也可以编辑它进行自定义，合并后依然会存在。
 
 ## 最后想说
 
--- toList.py
+++ toList.py
@@ -1,5 +1,5 @@
-# 本脚本用于GFWlist文件转IOS版shadowrocket配置文件
-import requests,base64
+# 本脚本用于 GFWlist 文件转IOS版 Shadowrocket 配置文件
+import requests,base64,datetime
 
 # 获取 GFWlist 文件
 try:
@@ -11,6 +11,20 @@ except Exception as err:
     print("获取GFWlist错误")
     print(err)
     exit()
+
+# 获取 GFWlist 更新时间
+try:
+    timestamp=urlList[5].split(": ")[1]
+    timef=timestamp[-5:]
+    lastModified = datetime.datetime.strptime(timestamp[:-6], '%a, %d %b %Y %H:%M:%S')
+    timei=int(timef[:3])
+    if timei >8:
+        lastModified -= datetime.timedelta(hours = timei - 8)
+    else :
+        lastModified += datetime.timedelta(hours = abs(8 - timei))
+    lastModified = '\n'+ '# 列表更新时间: ' + str(lastModified)
+except:
+    lastModified =""
 
 # 获取主域名
 domain = []
@@ -50,7 +64,7 @@ try:
         for line_list in f.readlines():
             # 在 [Rule] 下面新增规则
             if line_list == '[Rule]\n':
-                line_list = '[Rule]' + '\n' + '# GFWlist Start' +  con + '\n' + '# GFWlist End' + '\n'
+                line_list = '[Rule]' + '\n' + '# GFWlist Start' + lastModified + con + '\n' + '# GFWlist End' + '\n'
             ff.write(line_list)
     print('共写入 ' + str(len(domain)) + ' 个被墙域名规则')
 except Exception as err:
