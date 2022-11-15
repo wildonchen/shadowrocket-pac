f95df5297e63474d21508e4197f88adf1b5fe134
chenqiwei <me@chenqiwei.net>
2022-11-15 22:00:50 +0800

    优化匹配规则

--- tool/toList.py
+++ tool/toList.py
@@ -1,26 +1,13 @@
-# 本脚本用于 GFWlist 文件转IOS版 Shadowrocket 配置文件
-import requests,base64,random,re
+import requests,base64,re
 
-def txtContent(url,type=""):
+def txtContent(url,type="",split="\n"):
     ''' 获取远程文件 '''
-    user_agent_list = [
-        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.42",
-        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36",
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36",
-        "Mozilla/5.0 (Macintosh; U; PPC Mac OS X 10.5; en-US; rv:1.9.2.15) Gecko/20110303 Firefox/3.6.15",
-    ]
-    headers={
-        'User-Agent':random.choice(user_agent_list),'Connection':'close'
-    }
     print("开始获取远程文件")
     try:
-        content=requests.get(url,  headers=headers).content
+        content=requests.get(url).content
         if type == "base64":
             content=base64.b64decode(content)
-        textList = content.decode('utf-8').split("\n")
+        textList = content.decode('utf-8').split(split)
         return textList
     except Exception as err:
         print("获取远程文件失败")
@@ -34,7 +21,7 @@ def domainList(urlList):
     for i in range(0, len(urlList)):
         nowUrl = urlList[i]
         # 基本过滤
-        if ('@' not in nowUrl)  and ('#' not in nowUrl) and ('!' not in nowUrl) and ('$' not in nowUrl):
+        if ('@' not in nowUrl)  and ('#' not in nowUrl) and ('!' not in nowUrl) and ('$' not in nowUrl) and ('/' not in nowUrl):
             # 正则匹配
             pattern = re.compile(r"(?:[\w](?:[\w\-]{0,61}[\w])?\.)+[a-zA-Z]{2,6}")
             url = pattern.findall(nowUrl)
@@ -82,7 +69,7 @@ def onlyCFWlist():
 
 def onlyADBlock():
     print("ADBlock")
-    #ad= txtContent('https://raw.githubusercontent.com/easylist/easylistchina/master/easylistchina.txt') + txtContent('https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/rule.txt')
+    # ad= txtContent('https://raw.githubusercontent.com/easylist/easylistchina/master/easylistchina.txt') + txtContent('https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/rule.txt')
     ad = txtContent('https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_15_DnsFilter/filter.txt')
     con = conConfig(domainList(ad), 'Reject')
     con = '# ADBlock Start' + con + '\n' + '# ADBlock End'
@@ -100,7 +87,7 @@ def CFWlistAndADBlock():
 
 def onlyWhitelist():
     print("Whitelist")
-    con = conConfig(domainList(txtContent('https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf')),"DIRECT")
+    con = conConfig(domainList(txtContent('https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf', '','/')),"DIRECT")
     # 国内直连，国外代理
     con = '# Whitelist Start' + con + '\n' + '# Whitelist End' + '\n' + '# China' + '\n' + 'GEOIP,CN,DIRECT' + '\n' + '# Final' + '\n' + 'FINAL,PROXY'
     writeToConf('Whitelist.conf', con)
